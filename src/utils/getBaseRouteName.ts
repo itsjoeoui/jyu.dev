6e5d5921d3aa6326f1568efea8d5819590d2973e
Joey Yu <joey@itsjoeoui.com>
2023-09-10 23:49:37 -0400

    checkpoint

---
+++ src/utils/getBaseRouteName.ts
@@ -0,0 +1,5 @@
+export const getBaseRouteName = (pathname: string) => {
+  const matchTrailingSlash = /\w+\/$/;
+  if (matchTrailingSlash.test(pathname)) return pathname.slice(0, -1);
+  return pathname;
+};
