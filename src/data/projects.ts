fd7570415a6bb663efaa77cb4f402a6527011b61
Joey Yu <joey@itsjoeoui.com>
2023-09-12 15:36:36 -0400

    Update

---
+++ src/data/projects.ts
@@ -0,0 +1,16 @@
+import type { Project } from "../types/project";
+
+export const projectsData: Project[] = [
+  {
+    name: "Flojoy Studio",
+    desc: "Flojoy Studio is the open-source, Python-based alternative to engineering software like LabVIEW, Simulink, and Alteryx.",
+    link: "https://flojoy.ai",
+    tags: ["TypeScript", "React", "Python", "FastAPI"],
+  },
+  {
+    name: "Flojoy Cloud",
+    desc: "Engineers use Flojoy Studio to stream measurements from robotics, microcontrollers, single board computers, test stations, and benchtop instruments to Flojoy Cloud.",
+    link: "https://cloud.flojoy.ai",
+    tags: ["TypeScript", "Next.js", "FaunaDB"],
+  },
+];
---
+++ src/types/project.ts
@@ -0,0 +1,6 @@
+export type Project = {
+  name: string;
+  desc: string;
+  link: string;
+  tags: string[];
+};
