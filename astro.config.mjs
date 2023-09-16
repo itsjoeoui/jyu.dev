01bd21c14f6b5e27b0fc321802ff5a1d3111f0e0
Joey Yu <joey@itsjoeoui.com>
2023-09-15 22:23:08 -0400

    format and update

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,22 +11,34 @@ import react from "@astrojs/react";
 export default defineConfig({
   markdown: {
     remarkPlugins: [remarkToc],
-    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
-      behavior: "append",
-      content: {
-        type: "element",
-        tagName: "span",
-        properties: {
-          className: ["anchor-link", "group-hover:block", "group"]
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+          content: {
+            type: "element",
+            tagName: "span",
+            properties: {
+              className: ["anchor-link", "group-hover:block", "group"],
+            },
+            children: [
+              {
+                type: "text",
+                value: "#",
+              },
+            ],
+          },
         },
-        children: [{
-          type: "text",
-          value: "#"
-        }]
-      }
-    }]]
+      ],
+    ],
   },
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), mdx(), react()]
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx(),
+    react(),
+  ],
 });
--- irealizedineedabackend/main.go
+++ irealizedineedabackend/main.go
@@ -2,16 +2,43 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+type Post struct {
+	Data    time.Time `json:"date"`
+	Content string    `json:"content"`
+	ID      uint32    `json:"id"`
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+
+	posts := []Post{
+		{
+			ID:      0,
+			Content: "Hello World",
+			Data:    time.Now(),
+		},
+		{
+			ID:      1,
+			Content: "What time is it?",
+			Data:    time.Now(),
+		},
+		{
+			ID:      2,
+			Content: "All pointers should come first to optimze GC.",
+			Data:    time.Now(),
+		},
+	}
+
+	r.GET("/api/posts", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"data": posts,
 		})
 	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
