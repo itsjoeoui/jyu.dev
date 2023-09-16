e5d2d525b074127fbb4d8baf767d9eab637e8bef
Joey Yu <joey@itsjoeoui.com>
2023-09-15 21:53:44 -0400

    bring in react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,32 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import remarkToc from "remark-toc";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     remarkPlugins: [remarkToc],
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: "append",
-          content: {
-            type: "element",
-            tagName: "span",
-            properties: {
-              className: ["anchor-link", "group-hover:block", "group"],
-            },
-            children: [{ type: "text", value: "#" }],
-          },
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
+      behavior: "append",
+      content: {
+        type: "element",
+        tagName: "span",
+        properties: {
+          className: ["anchor-link", "group-hover:block", "group"]
         },
-      ],
-    ],
+        children: [{
+          type: "text",
+          value: "#"
+        }]
+      }
+    }]]
   },
-  integrations: [tailwind({ applyBaseStyles: false }), mdx()],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), mdx(), react()]
 });
