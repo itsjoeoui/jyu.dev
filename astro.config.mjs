50332ffd5638052519b311c40c108b2174c1ee7d
Joey Yu <joey@jyu.dev>
2023-09-22 15:12:39 -0400

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,47 +4,32 @@ import remarkToc from "remark-toc";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  adapter: vercel({"webAnalytics": true}),
   markdown: {
     remarkPlugins: [remarkToc],
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: "append",
-          content: {
-            type: "element",
-            tagName: "span",
-            properties: {
-              className: ["anchor-link", "group-hover:block", "group"],
-            },
-            children: [
-              {
-                type: "text",
-                value: "#",
-              },
-            ],
-          },
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
+      behavior: "append",
+      content: {
+        type: "element",
+        tagName: "span",
+        properties: {
+          className: ["anchor-link", "group-hover:block", "group"]
         },
-      ],
-    ],
+        children: [{
+          type: "text",
+          value: "#"
+        }]
+      }
+    }]]
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    mdx(),
-    react(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), mdx(), react()]
 });
