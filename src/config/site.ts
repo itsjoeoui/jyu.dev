ca71f02510d34d18618a3c1a4b4673be7dd76e2f
Joey Yu <joey@jyu.dev>
2024-03-21 10:45:26 -0400

    chore: add a type for config

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,4 +1,9 @@
-export const config = {
+type Config = {
+  email: string;
+  discordUserId: string;
+};
+
+export const config: Config = {
   email: "joey@jyu.dev",
   discordUserId: "481294021054955533",
 };
