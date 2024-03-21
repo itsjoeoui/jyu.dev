f97bd3d3138c2c55e385ff90f30fff3287272d9e
Joey Yu <joey@jyu.dev>
2024-03-20 23:33:42 -0400

    chore: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db(), react()]
+  integrations: [tailwind(), db(), react(), sitemap()]
 });
