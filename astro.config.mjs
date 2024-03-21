e189952b783b96559cafa6f1cd064081edd6adfa
Joey Yu <joey@jyu.dev>
2024-03-20 23:53:51 -0400

    chore: add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
 import react from "@astrojs/react";
@@ -7,5 +7,11 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db(), react(), sitemap()]
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    db(),
+    react(),
+    sitemap(),
+  ],
 });
+
