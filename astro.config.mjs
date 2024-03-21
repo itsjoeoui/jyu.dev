360fd35cd311ee5ea7e79437cd44e432f685995e
Joey Yu <joey@jyu.dev>
2024-03-20 23:31:33 -0400

    chore: add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import db from "@astrojs/db";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db()]
+  integrations: [tailwind(), db(), react()]
 });
