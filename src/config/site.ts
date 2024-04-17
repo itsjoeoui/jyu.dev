cc6f4a62c703b0ee8755342f1d993337406076f5
Joey Yu <joey@jyu.dev>
2024-04-17 18:31:54 -0400

    chore: add more content

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,12 +4,18 @@ type Config = {
   discordUserId: string;
   projects: Project[];
   contacts: Contact[];
+  tools: Tool[];
 };
 
 type Project = {
   title: string;
   description: string;
   link: string;
+};
+
+type Tool = {
+  name: string;
+  description: string;
 };
 
 type Contact = {
@@ -61,6 +67,43 @@ export const config: Config = {
       title: "Astro: Star Formation",
       description: "Physics simulation of star formation",
       link: "https://devpost.com/software/star-formation",
+    },
+  ],
+  tools: [
+    {
+      name: "Languages",
+      description:
+        "TypeScript, JavaScript, Golang, Python, Java, C/C++, Rust, OCaml, Clojure",
+    },
+    {
+      name: "Frontend",
+      description:
+        "React, Next.js, Astro, Docusaurus, HTML, CSS, TailwindCSS, Radix UI",
+    },
+    {
+      name: "Backend",
+      description:
+        "Chi, net/http, gRPC, Connect, FastAPI, Flask, ElysiaJS, NestJS, Express, GraphQL, Electron, tRPC, Tauri",
+    },
+    {
+      name: "Database",
+      description: "PostgreSQL, MySQL, MongoDB, SQLite, Redis, FaunaDB",
+    },
+    {
+      name: "AI",
+      description: "OpenAI, LangChain",
+    },
+    {
+      name: "Cloud",
+      description: "S3, EC2, AMI, SES",
+    },
+    {
+      name: "DevOps",
+      description: "Docker, GitHub Actions, GitLab Runners, Terraform",
+    },
+    {
+      name: "Editor",
+      description: "Neovim",
     },
   ],
   contacts: [
