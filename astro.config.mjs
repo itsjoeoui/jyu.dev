72b01f92e94cecca9f32441f1757729cf51ea49c
Joey Yu <joey@jyu.dev>
2024-04-17 20:09:43 -0400

    chore: nuke astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import db from "@astrojs/db";
 import react from "@astrojs/react";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({ applyBaseStyles: false }),
-    db(),
-    react(),
-    sitemap(),
-  ],
+  integrations: [tailwind({ applyBaseStyles: false }), react(), sitemap()],
 });
-
