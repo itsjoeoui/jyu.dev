90314ea03fe8b9a3e7913b22c3a619a9ff83c2dc
Joey Yu <joey@jyu.dev>
2024-12-25 21:52:00 -0500

    chore: format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,60 +1,59 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
-import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
-import remarkUnwrapImages from 'remark-unwrap-images'
-import rehypeExternalLinks from 'rehype-external-links'
-import expressiveCode from 'astro-expressive-code'
-import { expressiveCodeOptions } from './src/site.config'
-import icon from 'astro-icon'
-import vercel from '@astrojs/vercel/serverless'
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import { remarkReadingTime } from "./src/utils/remarkReadingTime.ts";
+import remarkUnwrapImages from "remark-unwrap-images";
+import rehypeExternalLinks from "rehype-external-links";
+import expressiveCode from "astro-expressive-code";
+import { expressiveCodeOptions } from "./src/site.config";
+import icon from "astro-icon";
+import vercel from "@astrojs/vercel";
 
-import partytown from '@astrojs/partytown'
+import partytown from "@astrojs/partytown";
 
-import sanity from '@sanity/astro'
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jyu.dev',
-	integrations: [
-		expressiveCode(expressiveCodeOptions),
-		tailwind({
-			applyBaseStyles: false
-		}),
-		sitemap(),
-		mdx(),
-		icon(),
-		partytown({ config: { forward: ['dataLayer.push'] } }),
-		sanity({
-			projectId: '9zcu1jb0',
-			dataset: 'production',
-			useCdn: false // for static builds
-		})
-	],
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-		rehypePlugins: [
-			[
-				rehypeExternalLinks,
-				{
-					target: '_blank',
-					rel: ['nofollow, noopener, noreferrer']
-				}
-			]
-		],
-		remarkRehype: {
-			footnoteLabelProperties: {
-				className: ['']
-			}
-		}
-	},
-	prefetch: true,
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true
-		}
-	})
-})
-
+  site: "https://jyu.dev",
+  integrations: [
+    expressiveCode(expressiveCodeOptions),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    mdx(),
+    icon(),
+    partytown({ config: { forward: ["dataLayer.push"] } }),
+    sanity({
+      projectId: "9zcu1jb0",
+      dataset: "production",
+      useCdn: false, // for static builds
+    }),
+  ],
+  markdown: {
+    remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: ["nofollow, noopener, noreferrer"],
+        },
+      ],
+    ],
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: [""],
+      },
+    },
+  },
+  prefetch: true,
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+});
