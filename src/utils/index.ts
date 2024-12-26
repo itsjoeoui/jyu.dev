839c6c698a9c8c842bb36b037998c241bcd5d779
Joey Yu <joey@jyu.dev>
2024-12-25 22:22:18 -0500

    chore: knip

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,4 +8,3 @@ export {
 export { getFormattedDate } from "./date";
 export { generateToc } from "./generateToc";
 export type { TocItem } from "./generateToc";
-export { elementHasClass, toggleClass, rootInDarkMode } from "./domElement";
