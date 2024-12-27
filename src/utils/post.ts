18356fae0ea4569351a3a0a2652f557418c7859d
Joey Yu <joey@jyu.dev>
2024-12-26 21:50:22 -0500

    fix: pagination

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -13,15 +13,15 @@ export async function getAllPosts() {
 
 export function sortMDByDate(posts: SanityDocument[]) {
   return posts.sort((a, b) => {
-    const aDate = new Date(a.data.updatedDate ?? a.data.publishDate).valueOf();
-    const bDate = new Date(b.data.updatedDate ?? b.data.publishDate).valueOf();
+    const aDate = new Date(a.publishedAt).valueOf();
+    const bDate = new Date(b.publishedAt).valueOf();
     return bDate - aDate;
   });
 }
 
 /** Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so. */
 export function getAllTags(posts: SanityDocument[]) {
-  return posts.flatMap((post) => [...post.tags]);
+  return posts.flatMap((post) => (post.tags ? [...post.tags] : []));
 }
 
 /** Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so. */
