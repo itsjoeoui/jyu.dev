2ac11a578c154cb63061234217c56efb8014f8c8
Joey Yu <joey@jyu.dev>
2024-12-27 00:23:32 -0500

    chore: config

--- src/site.config.ts
+++ src/site.config.ts
@@ -34,13 +34,17 @@ export const personalConfig: PersonalConfig = {
 
 export const menuLinks: Array<{ title: string; path: string }> = [
   {
-    title: "Home",
-    path: "/",
+    title: "Blog",
+    path: "/blog/",
   },
-  // {
-  // 	title: 'Blog',
-  // 	path: '/blog/'
-  // }
+  {
+    title: "Tools",
+    path: "/tools/",
+  },
+  {
+    title: "Referrals",
+    path: "/refer/",
+  },
 ];
 
 // https://expressive-code.com/reference/configuration/
